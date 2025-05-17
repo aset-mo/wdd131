afb3b0cdfe227de93a26d1164f07ff30ae4ec16d
aset-mo <imercadoortega@byupathway.edu>
2025-05-17 12:01:23 -0500

    Photo album

---
+++ scripts/temples.js
@@ -0,0 +1,10 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+document.getElementById('lastModified').textContent = document.lastModified;
+
+const menuButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+menuButton.addEventListener('click', () => {
+  navigation.classList.toggle('open');
+  menuButton.textContent = navigation.classList.contains('open') ? '✖' : '☰';
+});
